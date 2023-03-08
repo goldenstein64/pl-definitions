442505da324f6cc177fb713fc506e2ff60a1781e
goldenstein64 <goldnstein64@gmail.com>
2023-03-07 22:37:40 -0500

    Changed `warn` description

--- plugin.lua
+++ plugin.lua
@@ -573,8 +573,10 @@ local importCompatDiff = {
 	text = [[---Raise a warning message.
 ---
 ---This function mimics the `warn` function added in Lua 5.4.
+---
+---Calling `warn("@on")` and `warn("@off")` will turn on and off warnings 
+---respectively.
 ---@param ... any -- any arguments
----@overload fun(control: "@on" | "@off")
 function warn(...) end
 ]],
 }
