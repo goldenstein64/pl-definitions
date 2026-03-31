b37776d953b157c5deb57ff07d033fc27751d020
goldenstein64 <goldnstein64@gmail.com>
2026-03-30 20:52:26 -0400

    doc: add config usage

---
+++ examples/pl/config.lua
@@ -0,0 +1,29 @@
+local test_config = [[
+# test.config
+# Read timeout in seconds
+read.timeout=10
+# Write timeout in seconds
+write.timeout=5
+#acceptable ports
+ports = 1002,1003,1004
+]]
+do
+	local test_config_file = assert(io.open("test.config", "w"))
+	assert(test_config_file:write(test_config))
+	assert(test_config_file:close())
+end
+
+local config = require("pl.config")
+local pretty = require("pl.pretty")
+local t = assert(config.read("test.config"))
+
+assert(pretty.write(t) == [[
+{
+  ports = {
+    1002,
+    1003,
+    1004
+  },
+  write_timeout = 5,
+  read_timeout = 10
+}]])
