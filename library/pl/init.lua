90cfcc19975b01b37c7d1d58d29711276ab22e53
goldenstein64 <goldnstein64@gmail.com>
2026-04-05 17:33:02 -0400

    doc(init): add documentation

--- library/pl/init.lua
+++ library/pl/init.lua
@@ -1,4 +1,11 @@
 ---@meta
+---Entry point for loading all PL libraries only on demand, into the global
+---space. Requiring 'pl' means that whenever a module is implicitly accessed
+---(e.g. `utils.split`), then that module is dynamically loaded. The submodules
+---are all brought into the global space.
+
+-- this module doesn't return anything, so I'm just storing the `pl` global
+-- here.
 
 ---@class pl
 ---@field app pl.app
